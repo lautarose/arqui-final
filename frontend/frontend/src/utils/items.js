255526ec6af997583ff69e652e5b81b8ea936f2f
Julian Gergolet <juligergolet@gmail.com>
2023-06-28 17:59:43 -0300

    Front 2

---
+++ frontend/frontend/src/utils/items.js
@@ -0,0 +1,30 @@
+const products = [
+    {
+        "id":1,
+        "title":"Casa",
+        "seller":"Inmobiliaria",
+        "price":100000,
+        "currency":"usd",
+        "picture":"https://www.construyehogar.com/wp-content/uploads/2013/09/Fachada-casa-moderna-ancha.jpg",
+        "description":"Excelente casa en Barrio Nuevo Maurizzi, zona centro. A 500 mts de Dino Ruta 20 Complejo cerrado con acceso con portón automático",
+        "state":"Cordoba",
+        "city":"Cordoba",
+        "street":"San Martin",
+        "number":125
+    },
+    {
+        "id":2,
+        "title":"Chalet",
+        "seller":"RE/MAX",
+        "price":50000,
+        "currency":"usd",
+        "picture":"https://http2.mlstatic.com/chalet-medio-lote-57-n-682-fte-derecha-D_NQ_NP_980477-MLA27531047059_062018-F.jpg",
+        "description":"Este hermoso chalet se encuentra en La Falda, en la provincia de Córdoba, y alberga un jardín. Se encuentra a 200m de la avenida principal del Centro de La Falda y cuenta con  aparcamiento privado .",
+        "state":"cordoba",
+        "city":"La Falda",
+        "street":"Av. Eden",
+        "number":85
+    }
+];
+
+export default products;
