d03fc16446a70c9cddf9d61da8873dac0236ba7f
lautarose <1714186@ucc.edu.ar>
2023-06-30 18:46:14 -0300

    cors added

--- items-service/app/app.go
+++ items-service/app/app.go
@@ -1,9 +1,13 @@
 package app
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func StartApp() {
 	router := gin.Default()
+	router.Use(cors.Default())
 	deps := BuildDependencies()
 	MapUrls(router, deps)
 	_ = router.Run(":8090")
