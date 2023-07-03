90add0d46bee0faa7914a6bce217f3d028fe2bd5
Julian Gergolet <juligergolet@gmail.com>
2023-07-03 13:24:46 -0300

    Front version casi final

--- frontend/src/mypublics.js
+++ frontend/src/mypublics.js
@@ -5,6 +5,7 @@ const Cookie = new Cookies();
 const Publics = () => {
   const [token, setToken] = useState("");
   const [userId, setUserId] = useState("");
+  const [publications, setPublications] = useState([]);
 
   useEffect(() => {
     loadTokenFromCookie();
@@ -30,11 +31,27 @@ const Publics = () => {
       });
   }, [token]);
 
-    return (
-      <div>
-        <h1>User ID: {userId}</h1>
-      </div>
-    );
+  useEffect(() => {
+    fetch(`http://localhost:8090/items/user/${userId}`)
+      .then(response => response.json())
+      .then(data => {
+        setPublications(data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }, [userId, token]);
+
+  return (
+    <div>
+      <h1>User ID: {userId}</h1>
+      <ul>
+        {publications.map(publicationId => (
+          <li key={publicationId}>{publicationId}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Publics;
